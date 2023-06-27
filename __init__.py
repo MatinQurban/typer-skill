afc528fa9b32adc7e8833a10f7f22f9cae2b2953
MatinQurban <matinqurban@gmail.com>
2023-06-27 15:14:26 -0700

    changed init to incorporate different dialog

--- __init__.py
+++ __init__.py
@@ -1,12 +1,19 @@
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.util import play_mp3
 
 
 class Typer(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        
+
+    
 
     @intent_file_handler('typer.intent')
     def handle_typer(self, message):
+        self.speak_dialog('Waiting')
+        self.wait_while_speaking()
+        play_mp3('/home/pi/mycroft-core/mycroft/skills/typer-skill.mycroftai/typer.mp3')
         self.speak_dialog('typer')
 
 
